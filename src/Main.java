3fbe05ecf458aa305f3b9ff22d001f067a677aa3
HoussemLangar <140980709+HoussemLangar@users.noreply.github.com>
2025-09-19 11:44:26 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal lion = new Animal("Félidés", "Lion", 5, true);
+        Animal chat = new Animal("Félidés", "Chat", 3, true);
+        Animal chien = new Animal("Canidés", "Chien", 4, true);
+        Zoo myZoo = new Zoo("Safari Park", "Tunis", 25);
+
+        System.out.println("-------- Résultats --------");
+
+        System.out.println("Animal -> Nom: " + lion.getName() +
+                ", Famille: " + lion.getFamily() +
+                ", Âge: " + lion.getAge() +
+                ", Mammifère: " + lion.isMammal());
+
+        System.out.println("Zoo -> Nom: " + myZoo.getName() +
+                ", Ville: " + myZoo.getCity() +
+                ", Nombre de cages: " + myZoo.getNbrCages());
+
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+
+        System.out.println(chat);
+        System.out.println(chien.toString());
+    }
+}
